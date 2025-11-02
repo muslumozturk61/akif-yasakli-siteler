897405a8848854ffc055a22e0268a395064b0ac5
Müslüm <muslum.ozturk@hotmail.com>
2025-11-02 21:52:44 +0300

    _

--- kodlar/site_yasakla_otomatik.py
+++ kodlar/site_yasakla_otomatik.py
@@ -1,19 +1,64 @@
-import libs as l
+import requests, ctypes
+
+url = "https://raw.githubusercontent.com/muslumozturk61/akif-yasakli-siteler/refs/heads/main/siteler.txt"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
+
+hosts_file_path = r"C:\Windows\System32\drivers\etc\hosts"
+
+def isAdmin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def siteListesi(url=url, headers=headers):
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            linkler = []
+            for i in response.text.split("\n"):
+                if (i != ""):
+                    linkler.append(i)
+            return linkler
+        else:
+            return []
+    except Exception as e:
+        print(e) 
+        return []  
+    
+def readHostFile():
+    try:
+        with open(hosts_file_path, 'r', encoding="utf-8") as hosts_file:
+            return hosts_file.readlines()
+    except:
+        return []
+    
+def writeHostFile(text):
+    try:
+        with open(hosts_file_path, 'a', encoding="utf-8") as hosts_file:
+            hosts_file.write(text)
+            return True
+    except:
+        return False
+    
+
 
 hosts_file_path = r"C:\Windows\System32\drivers\etc\hosts"
 
 def ban(baning_url):
   
-    hosts_file_lines = l.readHostFile()
+    hosts_file_lines = readHostFile()
 
     for i in hosts_file_lines:
         if (i.__contains__(f"127.0.0.1 {baning_url}")):
             return None
         
-    l.writeHostFile(f"\n127.0.0.1 {baning_url}")
+    writeHostFile(f"\n127.0.0.1 {baning_url}")
         
-if (l.isAdmin()):
-    for i in l.siteListesi():
+if (isAdmin()):
+    for i in siteListesi():
         ban(i)
 else:
     print("yönetici hakları gerekli")
