230bb03177959b36f06bbda908361e2591b6d758
Müslüm <muslum.ozturk@hotmail.com>
2025-11-02 20:46:53 +0300

    _

--- kodlar/link_okuma.py
+++ kodlar/link_okuma.py
@@ -4,10 +4,18 @@ url = "https://raw.githubusercontent.com/muslumozturk61/akif-yasakli-siteler/ref
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
-
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-    print(response.text)
-else:
-    print(response.status_code)
+def siteListesi():
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            linkler = []
+            for i in response.text.split("\n"):
+                if (i != ""):
+                    linkler.append(i)
+            print(response.text)
+            return linkler
+        else:
+            return []
+    except Exception as e:
+        print(e) 
+        return []  
